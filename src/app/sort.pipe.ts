b66b1257e187e0c88a4a1f4ffed75ca29108a2ed
jagrititaneja <jagriti.taneja92@gmail.com>
2018-05-11 23:02:43 +0530

    changes

--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -1,16 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+import { AppHttpService } from './app-http.service';
 @Pipe({
   name: 'sort'
 })
-export class SortPipe implements PipeTransform {
 
+export class SortPipe implements PipeTransform {
+  constructor(public appHttpService: AppHttpService) { }
   transform(value: Array<any>, args?: any): any {
     value.sort((a: any, b: any) => {
-      if (a.name < b.name) {
+      let data1 = a.name ? a.name : a.aliases[0];
+      let data2 = b.name ? b.name : b.aliases[0];
+      if (data1 < data2 ) {
         return -1;
       }
-      else if (a.name > b.name) {
+      else if (data1> data2) {
         return 1;
       }
       else {
